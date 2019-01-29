34c9305670c380dd36155400158174e5c4128951
kianooshsoleimani <kianooshsoleimani@gmail.com>
2019-01-29 15:29:09 +0330

    add package

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# react-native-searchableDropDown
+# React Native Searchable DropDown
+
+a list of item with flatlist you can search on it and use one of them like dropdown
+
+## Features
+
+- simple flatlist dropdown searchable
+
+## Installation
+
+$ npm install react-native-searchableDropDown --save
+
+## Quick Start
+
+soonly add
+
---
+++ utility/Utils.js
@@ -0,0 +1,40 @@
+const Utils = {
+   
+    shallowEqual(objA: mixed, objB: mixed): boolean {
+        if (objA === objB) {
+            return true;
+        }
+
+        if (typeof objA !== 'object' || objA === null ||
+            typeof objB !== 'object' || objB === null) {
+            return false;
+        }
+
+        const keysA = Object.keys(objA);
+        const keysB = Object.keys(objB);
+
+        if (keysA.length !== keysB.length) {
+            return false;
+        }
+
+        // Test for A's keys different from B.
+        const bHasOwnProperty = hasOwnProperty.bind(objB);
+        for (let i = 0; i < keysA.length; i++) {
+            if (!bHasOwnProperty(keysA[i]) || objA[keysA[i]] !== objB[keysA[i]]) {
+                return false;
+            }
+        }
+        return true;
+    },
+
+   
+    shallowCompare(instance, nextProps, nextState) {
+        return (
+            !Utils.shallowEqual(instance.props, nextProps) ||
+            !Utils.shallowEqual(instance.state, nextState)
+        );
+    },
+
+};
+
+export default Utils;
