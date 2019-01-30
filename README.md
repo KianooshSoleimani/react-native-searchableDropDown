13d5a4ace6ed15bf637ce4dfea451e8602649d9a
kianooshsoleimani <kianooshsoleimani@gmail.com>
2019-01-30 10:22:06 +0330

    add example

--- README.md
+++ README.md
@@ -6,11 +6,79 @@ a list of item with flatlist you can search on it and use one of them like dropd
 
 - simple flatlist dropdown searchable
 
+![alt img](test.gif)
+
 ## Installation
 
+
+```sh
 $ npm install react-native-dropdown-searchable --save
+```
 
 ## Quick Start
 
-soonly add
+```js
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+import SearchableDropDown from 'react-native-dropdown-searchable';
 
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [
+        {
+          tagId: 1,
+          title: 'first item',
+        },
+        {
+          tagId: 2,
+          title: 'second item',
+        }
+      ],
+      tagItem: {
+        tagId: 1,
+        title: 'nothing'
+      }
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <SearchableDropDown
+          onTextChange={tag => {
+            this.setState({ tag });
+          }}
+          onItemSelect={item => {
+            this.setState({ tagItem: item });
+          }}
+          items={this.state.list}
+          defaultIndex={0}
+          resetValue={false}
+          placeholder={'type something...'}
+          placeholderTextColor={'black'}
+          underlineColorAndroid="transparent"
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
+
+```
+## Authors
+
+* **Kianoosh Soleimani** -(https://github.com/KianooshSoleimani)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- index.js
+++ index.js
@@ -1,1 +1,201 @@
-export * from './src/SearchableDropDown';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { FlatList, Keyboard, Text, TextInput, TouchableOpacity, View, ViewPropTypes, StyleSheet } from 'react-native';
+import normalize from './utility/NormalizedText';
+import { hp, wp } from './utility/ResponsiveScreen';
+import Utils from './utility/Utils';
+
+function styleByListSize(list) {
+    const length = list.length;
+    const height = hp('8.5%');
+    const style = {
+        maxHeight: hp('0%'),
+        borderWidth: 1
+    };
+    if (length < 6) {
+        style.maxHeight = length * height;
+    } else {
+        style.maxHeight = hp('50%');
+    }
+    if (length === 0) {
+        style.borderWidth = 0;
+    }
+    return style;
+}
+
+export default class SearchableDropDown extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            item: {},
+            listItems: [],
+            focus: false,
+        };
+    }
+
+    componentDidMount() {
+        const listItems = this.props.items;
+        const defaultIndex = this.props.defaultIndex;
+        if (defaultIndex && listItems.length > defaultIndex) {
+            this.setState({
+                listItems,
+                item: listItems[defaultIndex]
+            });
+        }
+        else {
+            this.setState({ listItems });
+        }
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return Utils.shallowCompare(this, nextProps, nextState);
+    }
+
+    searchedItems = (searchedText) => {
+        const ac = this.props.items.filter((item) => {
+            return item.title.indexOf(searchedText) > -1;
+        });
+        const item = {
+            title: searchedText
+        };
+        this.setState({ listItems: ac, item });
+        const onTextChange = this.props.onTextChange;
+        if (onTextChange && typeof onTextChange === 'function') {
+            setTimeout(() => {
+                onTextChange(searchedText);
+            }, 0);
+        }
+    };
+
+    renderItems(item) {
+        return (
+            <TouchableOpacity
+                style={searchableDropDownStyle.itemContainer}
+                onPress={() => {
+                    this.setState({ item, focus: false }, () => {
+                        this.props.onItemSelect(item);
+                    });
+                    Keyboard.dismiss();
+                }}
+            >
+                <Text style={searchableDropDownStyle.itemText}>
+                    {item.title}
+                </Text>
+            </TouchableOpacity>
+        );
+    }
+
+    renderFlatList() {
+        if (this.state.focus) {
+            return (
+                <FlatList
+                    keyboardShouldPersistTaps="always"
+                    data={this.state.listItems}
+                    keyExtractor={(item, index) => index.toString()}
+                    renderItem={({ item }) => this.renderItems(item)}
+                    style={[searchableDropDownStyle.listContainer, styleByListSize(this.state.listItems)]}
+                />
+            );
+        }
+    }
+
+    render() {
+        return (
+            <View style={searchableDropDownStyle.container}>
+                <TextInput
+                    underlineColorAndroid={this.props.underlineColorAndroid}
+                    maxLength={30}
+                    onFocus={() =>
+                        this.setState({ focus: true })
+                    }
+                    onBlur={() =>
+                        this.setState({ focus: false })
+                    }
+                    ref={(e) => { this.input = e; }}
+                    onChangeText={(text) =>
+                        this.searchedItems(text)
+                    }
+                    value={this.state.item.title}
+                    style={searchableDropDownStyle.input}
+                    placeholderTextColor={this.props.placeholderTextColor}
+                    placeholder={this.props.placeholder}
+                />
+                {this.renderFlatList()}
+            </View>
+        );
+    }
+
+}
+
+const searchableDropDownStyle = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'center',
+    },
+    listContainer: {
+        backgroundColor: 'transparent',
+        width: wp('80%'),
+        borderColor: 'gray',
+        borderRadius: 5,
+    },
+    itemContainer: {
+        height: hp('7%'),
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+        alignSelf: 'center',
+        margin: 5,
+        zIndex: 0,
+    },
+    itemText: {
+        fontSize: normalize(14),
+        color: 'black',
+        textAlign: 'right',
+        padding: 5,
+    },
+    input: {
+        height: hp('6%'),
+        width: wp('80%'),
+        alignSelf: 'center',
+        borderColor: 'black',
+        borderWidth: 0.6,
+        borderRadius: 5,
+        fontSize: normalize(14),
+        color: 'black',
+        textAlign: 'right',
+    }
+});
+
+SearchableDropDown.defaultProps = {
+    items: [],
+    defaultIndex: -1,
+    onTextChange: () => {},
+    onItemSelect: () => {},
+    containerStyle: {},
+    textInputStyle: {},
+    itemStyle: {},
+    itemTextStyle: {},
+    resetValue: true,
+    placeholder: 'لطفا متنی وارد کنید...',
+    placeholderTextColor: 'black',
+    itemsContainerStyle: {},
+    underlineColorAndroid: 'transparent',
+};
+
+SearchableDropDown.propTypes = {
+    items: PropTypes.array,
+    defaultIndex: PropTypes.number,
+    onTextChange: PropTypes.func,
+    onItemSelect: PropTypes.func,
+    containerStyle: ViewPropTypes.style,
+    textInputStyle: ViewPropTypes.style,
+    itemStyle: ViewPropTypes.style,
+    itemTextStyle: ViewPropTypes.style,
+    resetValue: PropTypes.bool,
+    placeholder: PropTypes.string,
+    placeholderTextColor: PropTypes.string,
+    itemsContainerStyle: ViewPropTypes.style,
+    underlineColorAndroid: PropTypes.string,
+};
